6701562002a83ae32523504eb06c549a93a89272
Diego <diegobc04@gmail.com>
2025-09-24 17:50:40 -0500

    adds eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,34 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import expoConfig from "eslint-config-expo/flat.js";
+import prettier from "eslint-config-prettier";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 
-module.exports = defineConfig([
+export default defineConfig([
   expoConfig,
+  pluginReact.configs.flat.recommended,
   {
-    ignores: ['dist/*'],
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "react/self-closing-comp": ["error", { component: true, html: true }],
+    },
+  },
+  prettier,
+  {
+    ignores: ["dist/", "node_modules/"],
   },
 ]);
