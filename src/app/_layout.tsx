5ce2470c52026737b975eb0fdbb011813c5bdf64
Diego <diegobc04@gmail.com>
2025-09-25 11:50:15 -0500

    refactors root layout file

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,26 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { PaperProvider } from "react-native-paper";
 import { AuthProvider, useAuthContext } from "../contexts/auth";
+import { LiveQuotesProvider } from "../contexts/liveQuotes";
+import { WatchlistProvider } from "../contexts/watchlist";
 import { SplashScreenController } from "../screens/Splash";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
     <AuthProvider>
       <SplashScreenController />
-      <InnerLayout />
+      <QueryClientProvider client={queryClient}>
+        <WatchlistProvider>
+          <LiveQuotesProvider>
+            <PaperProvider>
+              <InnerLayout />
+            </PaperProvider>
+          </LiveQuotesProvider>
+        </WatchlistProvider>
+      </QueryClientProvider>
     </AuthProvider>
   );
 }
@@ -15,20 +28,16 @@ export default function RootLayout() {
 function InnerLayout() {
   const { accessToken, isLoading } = useAuthContext();
 
-  if (isLoading) {
-    return null;
-  }
+  if (isLoading) return null;
 
   return (
-    <PaperProvider>
-      <Stack>
-        <Stack.Protected guard={!!accessToken}>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack.Protected>
-        <Stack.Protected guard={!accessToken}>
-          <Stack.Screen name="sign-in" options={{ headerShown: false }} />
-        </Stack.Protected>
-      </Stack>
-    </PaperProvider>
+    <Stack>
+      <Stack.Protected guard={!!accessToken}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack.Protected>
+      <Stack.Protected guard={!accessToken}>
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+      </Stack.Protected>
+    </Stack>
   );
 }
